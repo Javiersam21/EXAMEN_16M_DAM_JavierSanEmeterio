8c4f46e335f2f5a77430de9e57076ac5773babc5
ALUMNOS <ALUMNOS@P622.AULAS.COM>
2024-05-22 12:17:58 +0200

    Hola soy Javier

---
+++ src/Examen/Examen.java
@@ -0,0 +1,10 @@
+package Examen;
+
+public class Examen {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Buenos días Ceinmark , soy Javier San Emeterio Martínez");
+		System.out.println("Introduce un número para averiguar si es primo o no: ");
+	}
+
+}
